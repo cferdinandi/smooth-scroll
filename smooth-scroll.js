2d79c840d3c32cb3af0189c6069b0c73c02f5e5f
Todd Motto <todd@toddmotto.com>
2014-06-02 18:33:51 +0100

    Add trim method, refactor private fns to drop _ prefix, minor tweaks

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -26,13 +26,17 @@
 
 	var exports = {};
 
+	var trim = function (str) {
+		return str.replace(/^\s+|\s+$/g, '');
+	};
+
 	var supports = function () {
-		return 'querySelector' in document && 'addEventListener' in root && String.prototype.trim && Array.prototype.forEach;
+		return 'querySelector' in document && 'addEventListener' in root && Array.prototype.forEach;
 	};
 
 	// Default settings
 	// Private {object} variable
-	var _defaults = {
+	var defaults = {
 		speed: 500,
 		easing: 'easeInOutCubic',
 		offset: 0,
@@ -44,17 +48,19 @@
 	// Merge default settings with user options
 	// Private method
 	// Returns an {object}
-	var _mergeObjects = function ( original, updates ) {
-		for (var key in updates) {
-			original[key] = updates[key];
+	var extend = function ( target, source ) {
+		for (var key in source) {
+			if (Object.prototype.hasOwnProperty.call(source, key)) {
+				target[key] = source[key];
+			}
 		}
-		return original;
+		return target;
 	};
 
 	// Calculate the easing pattern
 	// Private method
 	// Returns a decimal number
-	var _easingPattern = function ( type, time ) {
+	var easingPattern = function ( type, time ) {
 		var pattern;
 		if ( type === 'easeInQuad' ) pattern = time * time; // accelerating from zero velocity
 		if ( type === 'easeOutQuad' ) pattern = time * (2 - time); // decelerating to zero velocity
@@ -74,7 +80,7 @@
 	// Calculate how far to scroll
 	// Private method
 	// Returns an integer
-	var _getEndLocation = function ( anchor, headerHeight, offset ) {
+	var getEndLocation = function ( anchor, headerHeight, offset ) {
 		var location = 0;
 		if (anchor.offsetParent) {
 			do {
@@ -89,7 +95,7 @@
 	// Determine the document's height
 	// Private method
 	// Returns an integer
-	var _getDocumentHeight = function () {
+	var getDocumentHeight = function () {
 		return Math.max(
 			document.body.scrollHeight, document.documentElement.scrollHeight,
 			document.body.offsetHeight, document.documentElement.offsetHeight,
@@ -100,16 +106,16 @@
 	// Convert data-options attribute into an object of key/value pairs
 	// Private method
 	// Returns an {object}
-	var _getDataOptions = function ( options ) {
-		var settings = {}; // Create settings object
+	var getDataOptions = function ( options ) {
+		var settings = {};
 		// Create a key/value pair for each setting
 		if ( options ) {
 			options = options.split(';');
 			options.forEach( function(option) {
-				option = option.trim();
+				option = trim(option);
 				if ( option !== '' ) {
 					option = option.split(':');
-					settings[option[0]] = option[1].trim();
+					settings[option[0]] = trim(option[1]);
 				}
 			});
 		}
@@ -119,10 +125,10 @@
 	// Update the URL
 	// Private method
 	// Runs functions
-	var _updateURL = function ( anchor, url ) {
+	var updateURL = function ( anchor, url ) {
 		if ( history.pushState && (url || url === 'true') ) {
 			history.pushState( {
-				pos:anchor.id
+				pos: anchor.id
 			}, '', anchor );
 		}
 	};
@@ -133,8 +139,8 @@
 	exports.animateScroll = function ( toggle, anchor, options, event ) {
 
 		// Options and overrides
-		options = _mergeObjects( _defaults, options || {} ); // Merge user options with defaults
-		var overrides = _getDataOptions( toggle ? toggle.getAttribute('data-options') : null );
+		options = extend( defaults, options || {} ); // Merge user options with defaults
+		var overrides = getDataOptions( toggle ? toggle.getAttribute('data-options') : null );
 		var speed = parseInt(overrides.speed || options.speed, 10);
 		var easing = overrides.easing || options.easing;
 		var offset = parseInt(overrides.offset || options.offset, 10);
@@ -144,10 +150,10 @@
 		var fixedHeader = document.querySelector('[data-scroll-header]'); // Get the fixed header
 		var headerHeight = fixedHeader === null ? 0 : (fixedHeader.offsetHeight + fixedHeader.offsetTop); // Get the height of a fixed header if one exists
 		var startLocation = root.pageYOffset; // Current location on the page
-		var endLocation = _getEndLocation( document.querySelector(anchor), headerHeight, offset ); // Scroll to location
+		var endLocation = getEndLocation( document.querySelector(anchor), headerHeight, offset ); // Scroll to location
 		var animationInterval; // interval timer
 		var distance = endLocation - startLocation; // distance to travel
-		var documentHeight = _getDocumentHeight();
+		var documentHeight = getDocumentHeight();
 		var timeLapsed = 0;
 		var percentage, position;
 
@@ -157,12 +163,12 @@
 		}
 
 		// Update URL
-		_updateURL(anchor, updateURL);
+		updateURL(anchor, updateURL);
 
 		// Stop the scroll animation when it reaches its target (or the bottom/top of page)
 		// Private method
 		// Runs functions
-		var _stopAnimateScroll = function (position, endLocation, animationInterval) {
+		var stopAnimateScroll = function (position, endLocation, animationInterval) {
 			var currentLocation = root.pageYOffset;
 			if ( position == endLocation || currentLocation == endLocation || ( (root.innerHeight + currentLocation) >= documentHeight ) ) {
 				clearInterval(animationInterval);
@@ -173,21 +179,21 @@
 		// Loop scrolling animation
 		// Private method
 		// Runs functions
-		var _loopAnimateScroll = function () {
+		var loopAnimateScroll = function () {
 			timeLapsed += 16;
 			percentage = ( timeLapsed / speed );
 			percentage = ( percentage > 1 ) ? 1 : percentage;
-			position = startLocation + ( distance * _easingPattern(easing, percentage) );
+			position = startLocation + ( distance * easingPattern(easing, percentage) );
 			root.scrollTo( 0, Math.floor(position) );
-			_stopAnimateScroll(position, endLocation, animationInterval);
+			stopAnimateScroll(position, endLocation, animationInterval);
 		};
 
 		// Set interval timer
 		// Private method
 		// Runs functions
-		var _startAnimateScroll = function () {
+		var startAnimateScroll = function () {
 			options.callbackBefore( toggle, anchor ); // Run callbacks before animating scroll
-			animationInterval = setInterval(_loopAnimateScroll, 16);
+			animationInterval = setInterval(loopAnimateScroll, 16);
 		};
 
 		// Reset position to fix weird iOS bug
@@ -197,7 +203,7 @@
 		}
 
 		// Start scrolling animation
-		_startAnimateScroll();
+		startAnimateScroll();
 
 	};
 
@@ -206,19 +212,17 @@
 	// Runs functions
 	exports.init = function ( options ) {
 
-		// Feature test before initializing
-		if ( supports() ) {
+		// feature test
+		if ( !supports() ) return;
 
-			// Selectors and variables
-			options = _mergeObjects( _defaults, options || {} ); // Merge user options with defaults
-			var toggles = document.querySelectorAll('[data-scroll]'); // Get smooth scroll toggles
+		// Selectors and variables
+		options = extend( defaults, options || {} ); // Merge user options with defaults
+		var toggles = document.querySelectorAll('[data-scroll]'); // Get smooth scroll toggles
 
-			// When a toggle is clicked, run the click handler
-			Array.prototype.forEach.call(toggles, function (toggle, index) {
-				toggle.addEventListener('click', exports.animateScroll.bind( null, toggle, toggle.getAttribute('href'), options ), false);
-			});
-
-		}
+		// When a toggle is clicked, run the click handler
+		Array.prototype.forEach.call(toggles, function (toggle, index) {
+			toggle.addEventListener('click', exports.animateScroll.bind( null, toggle, toggle.getAttribute('href'), options ), false);
+		});
 
 	};
 
