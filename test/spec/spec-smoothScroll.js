a8374d7bc31e55ca865952e96087860ba7c08cf3
Thibaud Colas <thibaudcolas@gmail.com>
2014-12-13 21:21:59 +1300

    Add simple public API tests

--- test/spec/spec-smoothScroll.js
+++ test/spec/spec-smoothScroll.js
@@ -1,9 +1,14 @@
 describe('Smooth Scroll', function () {
 
-  describe('init', function () {
-    it('should include the smoothScroll module', function () {
-      expect(!!smoothScroll).toBe(true);
-    });
-  });
+    describe('API', function () {
+        it('should export the smoothScroll module', function () {
+            expect(smoothScroll).toBeDefined();
+        });
 
+        it('should expose public functions', function () {
+            expect(smoothScroll.init).toEqual(jasmine.any(Function));
+            expect(smoothScroll.destroy).toEqual(jasmine.any(Function));
+            expect(smoothScroll.animateScroll).toEqual(jasmine.any(Function));
+        });
+    });
 });
