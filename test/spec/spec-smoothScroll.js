d58c098bd0506ba4838d19903437b8837b567564
Thibaud Colas <thibaudcolas@gmail.com>
2014-12-13 22:53:48 +1300

    Start testing click triggers

--- test/spec/spec-smoothScroll.js
+++ test/spec/spec-smoothScroll.js
@@ -10,5 +10,86 @@ describe('Smooth Scroll', function () {
             expect(smoothScroll.destroy).toEqual(jasmine.any(Function));
             expect(smoothScroll.animateScroll).toEqual(jasmine.any(Function));
         });
+
+        it('uses event listeners', function () {
+            spyOn(document, 'addEventListener');
+            smoothScroll.init();
+            expect(document.addEventListener).toHaveBeenCalledWith('click', jasmine.any(Function), false);
+
+            spyOn(document, 'removeEventListener');
+            smoothScroll.destroy();
+            expect(document.removeEventListener).toHaveBeenCalledWith('click', jasmine.any(Function), false);
+        });
+    });
+
+    /**
+    * Simulate a click event.
+    * @public
+    * @param {Element} the element to simulate a click on
+    */
+    var simulateClick = function (elt) {
+        var click = document.createEvent('MouseEvents');
+        click.initMouseEvent('click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+        elt.dispatchEvent(click);
+    };
+
+    /**
+    * Create a link element, add it to the body.
+    * @public
+    * @param {String} the href attribute of the new link
+    * @param {Boolean} whether to add data-scroll to the link or not
+    * @returns {Element}
+    */
+    var createTestLink = function (href, smooth) {
+        var elt = document.createElement('a');
+        elt.href = href;
+        if (smooth) {
+            elt.setAttribute('data-scroll', true);
+        }
+        document.body.appendChild(elt);
+        return elt;
+    };
+
+    // A pattern for settings to validate their format.
+    var settingsStub = {
+        speed: jasmine.any(Number),
+        easing: jasmine.any(String),
+        offset: jasmine.any(Number),
+        updateURL: jasmine.any(Boolean),
+        callbackBefore: jasmine.any(Function),
+        callbackAfter: jasmine.any(Function)
+    };
+
+    describe('click on anchor', function () {
+        var elt = createTestLink('#anchor', true);
+        document.body.setAttribute('id', 'anchor');
+
+        afterEach(function () {
+            smoothScroll.destroy();
+        });
+
+        it('triggers smooth scrolling', function () {
+            spyOn(smoothScroll, 'animateScroll');
+            smoothScroll.init();
+            simulateClick(elt);
+            // TODO: uncomment one, remove the other and .tohaveBeenCalled when the animateScroll signature bug is fixed.
+            // expect(smoothScroll.animateScroll).toHaveBeenCalledWith(elt, '#anchor', jasmine.objectContaining(settingsStub), jasmine.any(Object));
+            // expect(smoothScroll.animateScroll).toHaveBeenCalledWith(elt, '#anchor', jasmine.objectContaining(settingsStub));
+            expect(smoothScroll.animateScroll).toHaveBeenCalled();
+        });
+
+        it('does nothing if not initialized', function () {
+            spyOn(smoothScroll, 'animateScroll');
+            simulateClick(elt);
+            expect(smoothScroll.animateScroll).not.toHaveBeenCalled();
+        });
+
+        it('does nothing if destructed', function () {
+            spyOn(smoothScroll, 'animateScroll');
+            smoothScroll.init();
+            smoothScroll.destroy();
+            simulateClick(elt);
+            expect(smoothScroll.animateScroll).not.toHaveBeenCalled();
+        });
     });
 });
