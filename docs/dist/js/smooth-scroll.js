a541227605cc46f05c87748cda68397aa961a8eb
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2017-04-07 12:39:24 -0400

    Agarzola master (#318)
    * Track jshint as dev dependency
    
    gulp-jshint requires jshint@2.x as a peer dep,
    which wasn’t tracked.
    
    * Support offset function
    
    * Bump version & build dist
    
    * Update README for offset function
    
    * Updated dependencies
    
    * Updated dependencies

--- docs/dist/js/smooth-scroll.js
+++ docs/dist/js/smooth-scroll.js
@@ -1,6 +1,6 @@
 /*!
- * smooth-scroll v10.2.1: Animate scrolling to anchor links
- * (c) 2016 Chris Ferdinandi
+ * smooth-scroll v10.3.1: Animate scrolling to anchor links
+ * (c) 2017 Chris Ferdinandi
  * MIT License
  * http://github.com/cferdinandi/smooth-scroll
  */
@@ -342,7 +342,7 @@
 			// Get the height of a fixed header if one exists and not already set
 			headerHeight = getHeaderHeight( fixedHeader );
 		}
-		var endLocation = isNum ? anchor : getEndLocation( anchorElem, headerHeight, parseInt(animateSettings.offset, 10) ); // Location to scroll to
+		var endLocation = isNum ? anchor : getEndLocation( anchorElem, headerHeight, parseInt((typeof animateSettings.offset === 'function' ? animateSettings.offset() : animateSettings.offset), 10) ); // Location to scroll to
 		var distance = endLocation - startLocation; // distance to travel
 		var documentHeight = getDocumentHeight();
 		var timeLapsed = 0;
