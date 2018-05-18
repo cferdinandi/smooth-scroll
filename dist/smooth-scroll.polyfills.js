69e93a52bc22c0dbef5ad904917fbe1e66f336bc
cferdinandi <chris@gomakethings.com>
2018-05-18 11:28:40 -0400

    Pass anchor and toggle into offset() Addresses... - https://github.com/cferdinandi/smooth-scroll/issues/410 - https://github.com/cferdinandi/smooth-scroll/issues/386 - https://github.com/cferdinandi/smooth-scroll/issues/367

--- dist/smooth-scroll.polyfills.js
+++ dist/smooth-scroll.polyfills.js
@@ -1,5 +1,5 @@
 /*!
- * smooth-scroll v14.0.1: Animate scrolling to anchor links
+ * smooth-scroll v14.1.0: Animate scrolling to anchor links
  * (c) 2018 Chris Ferdinandi
  * MIT License
  * http://github.com/cferdinandi/smooth-scroll
@@ -475,7 +475,7 @@ if (window.Element && !Element.prototype.closest) {
 				// Get the height of a fixed header if one exists and not already set
 				headerHeight = getHeaderHeight(fixedHeader);
 			}
-			var endLocation = isNum ? anchor : getEndLocation(anchorElem, headerHeight, parseInt((typeof animateSettings.offset === 'function' ? animateSettings.offset() : animateSettings.offset), 10)); // Location to scroll to
+			var endLocation = isNum ? anchor : getEndLocation(anchorElem, headerHeight, parseInt((typeof animateSettings.offset === 'function' ? animateSettings.offset(anchor, toggle) : animateSettings.offset), 10)); // Location to scroll to
 			var distance = endLocation - startLocation; // distance to travel
 			var documentHeight = getDocumentHeight();
 			var timeLapsed = 0;
